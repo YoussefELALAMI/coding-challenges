7acadc686c0c4646561a7a045f3eee4eda581540
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-08-14 21:13:37 +0200

    Time: 658 ms (52.74%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,12 @@
+SELECT 
+    s.user_id,
+    ROUND(
+        AVG(
+            IF (c.action = 'confirmed', 1.00, 0.00)
+        ),
+        2
+    ) confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id
