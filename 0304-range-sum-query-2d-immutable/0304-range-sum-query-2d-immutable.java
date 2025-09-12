b62c89104a906fc79739b31a37ff51269613f81f
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-09-12 18:25:55 +0200

    Time: 116 ms (24.35%), Space: 65.5 MB (93.86%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -3,16 +3,24 @@ class NumMatrix {
     int[][] data;
     public NumMatrix(int[][] matrix) {
         this.data = matrix;
+        this.getPrefixSum();
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
         int total = 0;
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j ++){
-                total += data[i][j];
-            }
+        for (int i = row1; i <= row2; i ++){
+            if (col1 != 0) total += (data[i][col2] - data[i][col1-1]);
+            else total += data[i][col2];
         }
         return total;        
+    }
+
+    private void getPrefixSum() {
+        for (int i = 0; i < this.data.length; i++){
+            for (int j = 1; j < this.data[i].length; j ++){
+                data[i][j] += data[i][j-1];
+            }
+        }
     }
 }
 
