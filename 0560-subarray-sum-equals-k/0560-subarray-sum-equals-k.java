0a138f608e440ed8642e80a64c77574635ad8c51
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-09-14 17:18:51 +0200

    Time: 25 ms (47.8%), Space: 47.1 MB (10.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,5 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int prefixSum = 0, occurence = 0;
+        Map<Integer, Integer> prefixTable = new HashMap<>();
+        prefixTable.put(prefixSum, 1);
+        for(int i = 0; i < nums.length; i++){
+            prefixSum += nums[i];
+            int complement = prefixSum - k;
+            if(prefixTable.containsKey(complement)) occurence += prefixTable.get(complement);
+            prefixTable.put(prefixSum, prefixTable.getOrDefault(prefixSum, 0) + 1);
+        } 
+        return occurence;
+    }
+    /**
+    Brute Force :
         int total = 0;
         for(int i = 0; i<nums.length; i++){
             if(nums[i]==k) total++;
@@ -12,5 +25,5 @@ class Solution {
             }
         }
         return total;
-    }
+     */
 }
