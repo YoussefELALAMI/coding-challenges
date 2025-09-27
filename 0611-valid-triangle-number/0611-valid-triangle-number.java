7ba52c130421e1c72da8768bf8a32f05c6fb1f51
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-09-27 13:37:29 +0200

    Time: 187 ms (13.91%), Space: 42.9 MB (99.63%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,15 +1,25 @@
 class Solution {
     public int triangleNumber(int[] nums) {
         int numOftriplet = 0;
+        int n = nums.length;
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                for(int k = j+1; k < nums.length; k++){
-                    int comb1 = nums[i] + nums[j];
-                    if(comb1 > nums[k]) numOftriplet++;
-                }
+        for(int i = 0; i < n - 2; i++){
+            for(int j = i + 1; j < n - 1; j++){
+                int comb = nums[i] + nums[j];
+                int k = binarySearch(nums, comb, j+1);
+                numOftriplet += k - j - 1;
             }
         }
         return numOftriplet;
+    }
+
+    private int binarySearch(int[] nums, int target, int start){
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (end + start) / 2;
+            if(nums[mid] >= target) end = mid - 1;
+            else start = mid + 1;
+        }
+        return start;
     }
 }
