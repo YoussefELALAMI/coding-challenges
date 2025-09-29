77ad0d492170e76259cbe5561c69c813140cf3e3
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-09-29 13:25:31 +0200

    Time: 12 ms (9.29%), Space: 44.6 MB (5.57%) - LeetHub

---
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -0,0 +1,31 @@
+
+
+import static java.lang.Math.min;
+
+class Solution {
+    int n;
+    int[] values;
+    Map<Integer, Integer> memo = new HashMap<Integer, Integer>();
+    public int minScoreTriangulation(int[] values) {
+        this.n = values.length;
+        this.values = values;
+        return dp(0, n-1);
+    }
+
+    private int dp(int start, int end){
+        if(start+1 == end) return 0;
+        if(start+2 == end) return values[start] * values[start+1] * values[end];
+        int key = start * n + end;
+        int minScore = Integer.MAX_VALUE;
+        if(!memo.containsKey(key)){
+            for(int k = start+1; k < end; k++){
+                minScore = min(
+                    minScore,
+                    dp(start, k) + dp(k, end) + values[start] * values[k] * values[end]
+                    );
+            }
+            memo.put(key, minScore);
+        }
+        return memo.get(key);
+    }
+}
