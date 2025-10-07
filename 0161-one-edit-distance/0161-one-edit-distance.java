de4f3c441abb32ecbdc898198f064296cecbe15c
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-10-07 18:34:34 +0200

    Time: 1 ms (58.63%), Space: 42.2 MB (57.74%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.java
+++ 0161-one-edit-distance/0161-one-edit-distance.java
@@ -1,11 +1,33 @@
 class Solution {
     public boolean isOneEditDistance(String s, String t) {
+        if(s.equals("") && t.equals("")) return false;
+        if(s.equals(t)) return false;
+        if(Math.abs(s.length() - t.length()) > 1) return false;
+
+        String s1 = s.length() < t.length() ? s : t;
+        String s2 = s.length() < t.length() ? t : s;
+
+        int index1 = 0, index2 = 0;
+        boolean foundDiff = false;
+        while (index1 < s1.length() && index2 < s2.length()) {
+            if(s1.charAt(index1) != s2.charAt(index2)){
+                if (foundDiff) return false;
+                foundDiff = true;
+                if (s.length() == t.length()) index1++;
+            } else index1++;
+            index2++;
+        }
+        return true;
+    }
+
+/**
+* Brute Force Implementation :
+    public boolean isOneEditDistance(String s, String t) {
         if(s.length() == t.length()) return checkReplace(s, t);
         else if(s.length() - 1 == t.length()) return checkInsertOrRemove(t, s);
         else if(s.length() + 1 == t.length()) return checkInsertOrRemove(s, t);
         return false;
     }
-
     private boolean checkInsertOrRemove(String s, String t) {
         int index1 = 0, index2 = 0;
         while (index1 < s.length() && index2 < t.length()) {
@@ -31,4 +53,5 @@ class Solution {
         }
         return foundDiff;
     }
+*/
 }
