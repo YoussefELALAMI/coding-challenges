5ea132b048bdf4d33d61237afb63913035e8b3c1
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-10-07 14:26:17 +0200

    Time: 0 ms (100%), Space: 41.8 MB (10.35%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -1,13 +1,26 @@
 class Solution {
     public boolean canPermutePalindrome(String s) {
-        int countOdd = 0;
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            freqMap.put(c, freqMap.getOrDefault(c,0)+1);
-            if(freqMap.get(c) % 2 == 1) countOdd++;
-            else countOdd--;
+        /* Using bit Vector */
+        int bitVector = 0;
+        for(char c : s.toCharArray()){
+            int val = Character.getNumericValue(c);
+            int mask = 1 << val;
+            if ((bitVector & mask)==0) bitVector |= mask;
+            else bitVector &= ~mask;
         }
-        return countOdd <= 1;
+        boolean oneOddNum = (bitVector & (bitVector - 1)) == 0;
+        return bitVector == 0 || oneOddNum;
     }
 }
+/**
+ * Using Hash Table :
+    int countOdd = 0;
+    Map<Character, Integer> freqMap = new HashMap<>();
+    for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        freqMap.put(c, freqMap.getOrDefault(c,0)+1);
+        if(freqMap.get(c) % 2 == 1) countOdd++;
+        else countOdd--;
+    }
+    return countOdd <= 1;
+ */
