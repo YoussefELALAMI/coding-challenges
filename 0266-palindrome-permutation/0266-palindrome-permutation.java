609102cc2d70e2ec929b33cf7150741ddaa3bfcc
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-10-07 14:29:47 +0200

    Time: 0 ms (100%), Space: 41.4 MB (52.37%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -3,7 +3,8 @@ class Solution {
         /* Using bit Vector */
         int bitVector = 0;
         for(char c : s.toCharArray()){
-            int val = Character.getNumericValue(c);
+            int val = getCharNumber(c);
+            if(val < 0) continue;
             int mask = 1 << val;
             if ((bitVector & mask)==0) bitVector |= mask;
             else bitVector &= ~mask;
@@ -11,6 +12,14 @@ class Solution {
         boolean oneOddNum = (bitVector & (bitVector - 1)) == 0;
         return bitVector == 0 || oneOddNum;
     }
+    private int getCharNumber(char c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(Character.toLowerCase(c));
+        if (a <= val && val <= z) return val - a;
+        return -1; // non-letter characters
+    }
+
 }
 /**
  * Using Hash Table :
