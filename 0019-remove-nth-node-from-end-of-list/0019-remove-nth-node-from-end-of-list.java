3d777f4654e3cea7363520bccdfb4fc600fd906a
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-10-31 12:35:38 +0100

    Time: 0 ms (100%), Space: 43.4 MB (7.92%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -9,6 +9,36 @@
  * }
  */
 class Solution {
+    // Iterative Approach : Two Pointers
+    // Time Complexity : O(N)
+    // Space Complexity : O(1)
+    int count = 0;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode left = dummy;
+        ListNode right = dummy;
+        
+        for(int i = 0; i <= n; i++){
+            if(right == null) return null;
+            right = right.next;
+        }
+
+        while(right != null){
+            right = right.next;
+            left = left.next;
+        }
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
+
+
+/**
+ Recursive Approach :
+ Time Complexity : O(N)
+ Space Complexity : O(N)
+ class Solution {
     int count = 0;
     public ListNode removeNthFromEnd(ListNode head, int n) {
         return nthToLast(head, n);
@@ -26,3 +56,4 @@ class Solution {
         return head;
     }
 }
+ */
