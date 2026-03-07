e50b0e9a06aebcd5f5b71d638d9c90166df2ca53
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-03-07 15:55:15 +0100

    Time: 1 ms (54.31%), Space: 42.8 MB (50.48%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -1,35 +1,26 @@
 class Solution {
     public boolean canPermutePalindrome(String s) {
-        /* Using bit Vector */
-        int bitVector = 0;
-        for(char c : s.toCharArray()){
-            int val = getCharNumber(c);
-            if(val < 0) continue;
-            int mask = 1 << val;
-            if ((bitVector & mask)==0) bitVector |= mask;
-            else bitVector &= ~mask;
-        }
-        boolean oneOddNum = (bitVector & (bitVector - 1)) == 0;
-        return bitVector == 0 || oneOddNum;
-    }
-    private int getCharNumber(char c) {
-        int a = Character.getNumericValue('a');
-        int z = Character.getNumericValue('z');
-        int val = Character.getNumericValue(Character.toLowerCase(c));
-        if (a <= val && val <= z) return val - a;
-        return -1; // non-letter characters
-    }
+        int realLength = s.length();
+        boolean isEven = realLength%2 == 0;
+        HashMap<Character, Integer> occ = new HashMap<>();
 
-}
-/**
- * Using Hash Table :
-    int countOdd = 0;
-    Map<Character, Integer> freqMap = new HashMap<>();
-    for(int i = 0; i < s.length(); i++){
-        char c = s.charAt(i);
-        freqMap.put(c, freqMap.getOrDefault(c,0)+1);
-        if(freqMap.get(c) % 2 == 1) countOdd++;
-        else countOdd--;
+        for(char c : s.toCharArray()) occ.put(c, occ.getOrDefault(c, 0) + 1);
+
+        if(isEven){
+            for(Integer v: occ.values()){
+                if (v%2 != 0) {
+                    return false;
+                }
+            }
+        } else {
+            int countOdd = 0;
+            for(Integer v: occ.values()){
+                if(v%2 != 0){
+                    if(countOdd >= 1) return false;
+                    else countOdd++;
+                }
+            }
+        }
+        return true;
     }
-    return countOdd <= 1;
- */
+}
