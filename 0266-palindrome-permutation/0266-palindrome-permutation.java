0fe06bba0b0d9d19d704fd992cc4bcf2155ffa44
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-03-07 16:08:09 +0100

    Time: 1 ms (54.31%), Space: 42.9 MB (19.81%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean canPermutePalindrome(String s) {
-        int realLength = s.length();
-        boolean isEven = realLength%2 == 0;
         HashMap<Character, Integer> occ = new HashMap<>();
         int countOdd = 0;
 
         for(char c : s.toCharArray()) occ.put(c, occ.getOrDefault(c, 0) + 1);
 
         for(Integer v: occ.values()){
-            if (v%2 != 0) {
-                if(isEven) return false;
-                else if(!isEven && countOdd == 1) return false;
-                else countOdd++;
-            }
+            if (v%2 != 0) countOdd++;
         }
-        return true;
+        return countOdd <= 1;
     }
 }
