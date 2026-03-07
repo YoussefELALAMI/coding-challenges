0fa5834d346a0cc2b54d9433a7e0cf1d9fbb1310
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-03-07 16:00:21 +0100

    Time: 1 ms (54.31%), Space: 42.9 MB (19.81%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -3,22 +3,15 @@ class Solution {
         int realLength = s.length();
         boolean isEven = realLength%2 == 0;
         HashMap<Character, Integer> occ = new HashMap<>();
+        int countOdd = 0;
 
         for(char c : s.toCharArray()) occ.put(c, occ.getOrDefault(c, 0) + 1);
 
-        if(isEven){
-            for(Integer v: occ.values()){
-                if (v%2 != 0) {
-                    return false;
-                }
-            }
-        } else {
-            int countOdd = 0;
-            for(Integer v: occ.values()){
-                if(v%2 != 0){
-                    if(countOdd >= 1) return false;
-                    else countOdd++;
-                }
+        for(Integer v: occ.values()){
+            if (v%2 != 0) {
+                if(isEven) return false;
+                else if(!isEven && countOdd == 1) return false;
+                else countOdd++;
             }
         }
         return true;
