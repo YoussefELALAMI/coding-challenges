3604f4c06f8d90fd8c357fee1311dd577fb0ea32
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-03-08 14:07:38 +0100

    Time: 0 ms (100%), Space: 42.8 MB (33.55%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.java
+++ 0266-palindrome-permutation/0266-palindrome-permutation.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean canPermutePalindrome(String s) {
+        //return hashMapSolution(s);
+        return bitManipulationSolution(s);
+    }
+
+    private boolean hashMapSolution(String s) {
         HashMap<Character, Integer> occ = new HashMap<>();
         int countOdd = 0;
 
@@ -9,5 +14,21 @@ class Solution {
             if (v%2 != 0) countOdd++;
         }
         return countOdd <= 1;
+    }
+
+    private boolean bitManipulationSolution(String s){
+        // Create the Bit Vector
+        int bitVector = 0;
+
+        // Toggle the letters positions
+        for(char c: s.toCharArray()){
+            int index = c - 'a';
+            bitVector ^= (1 << index);
+        }
+
+        // Check at most one bit is set
+        boolean zeroBitSet = bitVector == 0;
+        boolean oneBitSet = (bitVector & (bitVector - 1)) == 0;
+        return zeroBitSet || oneBitSet;
     }
 }
