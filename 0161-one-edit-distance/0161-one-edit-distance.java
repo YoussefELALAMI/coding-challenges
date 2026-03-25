b30142d1b50f9b64cf24c1984df103ac06a35bf1
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-03-25 21:51:18 +0100

    Time: 2 ms (19.79%), Space: 43.7 MB (25.78%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.java
+++ 0161-one-edit-distance/0161-one-edit-distance.java
@@ -1,57 +1,37 @@
 class Solution {
     public boolean isOneEditDistance(String s, String t) {
-        if(s.equals("") && t.equals("")) return false;
-        if(s.equals(t)) return false;
-        if(Math.abs(s.length() - t.length()) > 1) return false;
-
-        String s1 = s.length() < t.length() ? s : t;
-        String s2 = s.length() < t.length() ? t : s;
-
-        int index1 = 0, index2 = 0;
-        boolean foundDiff = false;
-        while (index1 < s1.length() && index2 < s2.length()) {
-            if(s1.charAt(index1) != s2.charAt(index2)){
-                if (foundDiff) return false;
-                foundDiff = true;
-                if (s.length() == t.length()) index1++;
-            } else index1++;
-            index2++;
+        if (s.length() == t.length()) {
+            return replace(s, t);
+        } else if (s.length() - t.length() == 1){
+            return insert(t, s);
+        } else if (t.length() - s.length() == 1){
+            return insert(s, t);
         }
-        return true;
-    }
-
-/**
-* Brute Force Implementation :
-    public boolean isOneEditDistance(String s, String t) {
-        if(s.length() == t.length()) return checkReplace(s, t);
-        else if(s.length() - 1 == t.length()) return checkInsertOrRemove(t, s);
-        else if(s.length() + 1 == t.length()) return checkInsertOrRemove(s, t);
         return false;
     }
-    private boolean checkInsertOrRemove(String s, String t) {
-        int index1 = 0, index2 = 0;
-        while (index1 < s.length() && index2 < t.length()) {
-            if(s.charAt(index1) != t.charAt(index2)){
-                if(index1 != index2) return false;
-                index2++;
+
+    private boolean replace(String s1, String s2){
+        boolean oneEdit = false;
+        for(int i = 0; i < s1.length(); i++){
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (oneEdit) return false;
+                oneEdit = true;
+            }
+        }
+        return oneEdit;
+    }
+
+    private boolean insert(String s1, String s2){
+        int left = 0, right = 0;
+        while (left < s1.length() && right < s2.length()) {
+            if(s1.charAt(left) == s2.charAt(right)){
+                left++;
+                right++;
             } else {
-                index1++;
-                index2++;
+                if(left != right) return false;
+                right++;
             }
         }
         return true;
     }
-    
-
-    private boolean checkReplace(String s, String t) {
-        boolean foundDiff = false;
-        if (t.length() == s.length()) {
-            for(int i = 0; i < t.length(); i++){
-                if (foundDiff && t.charAt(i) != s.charAt(i)) return false;
-                if(t.charAt(i) != s.charAt(i)) foundDiff = true;
-            }
-        }
-        return foundDiff;
-    }
-*/
 }
