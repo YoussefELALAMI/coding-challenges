f6e05282c3feb13374165f70cd25f32d7d4a6007
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-04-25 12:43:17 +0200

    Time: 3 ms (17.62%), Space: 47.5 MB (54.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,17 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<List<Integer>> coordinates = new ArrayList<>();
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> columns = new HashSet<>();
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
-                if(matrix[i][j] == 0) coordinates.add(new ArrayList<>(Arrays.asList(i,j)));
+                if(matrix[i][j] == 0) {
+                    rows.add(i);
+                    columns.add(j);
+                }
             }
         }
-        for(int c = 0; c < coordinates.size(); c++){
-            int row = coordinates.get(c).get(0);
-            int column = coordinates.get(c).get(1);
+        for(int row : rows){
             for(int col = 0; col < matrix[0].length; col++){
                 matrix[row][col] = 0;
             }
+        }
+        for(int column : columns){
             for(int r = 0; r < matrix.length; r++){
                 matrix[r][column] = 0;
             }
