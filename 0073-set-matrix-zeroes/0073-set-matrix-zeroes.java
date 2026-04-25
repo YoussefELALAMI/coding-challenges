36dc0758492d91369ecc5ec4c5e381fdaf9ec67f
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-04-25 13:03:24 +0200

    Time: 1 ms (90.67%), Space: 47.4 MB (72.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,62 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rows = new HashSet<>();
-        Set<Integer> columns = new HashSet<>();
+        boolean rowHasZero = false;
+        boolean columnHasZero = false;
+
+        // Check if the first row has a zero
         for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
+            if(matrix[i][0] == 0){
+                columnHasZero = true;
+                break;
+            }
+        } 
+
+        // Check if the first column has a zero
+        for(int i = 0; i < matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                rowHasZero = true;
+                break;
+            }
+
+        }
+
+        // Check for zeroes in the rest of the array
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 1; j < matrix[i].length; j++){
                 if(matrix[i][j] == 0) {
-                    rows.add(i);
-                    columns.add(j);
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int row : rows){
-            for(int col = 0; col < matrix[0].length; col++){
-                matrix[row][col] = 0;
-            }
+        
+        // Nullify rows based on values in first column
+        for(int row = 1; row < matrix.length; row++){
+            if(matrix[row][0] == 0) nullifyRow(matrix, row);
         }
-        for(int column : columns){
-            for(int r = 0; r < matrix.length; r++){
-                matrix[r][column] = 0;
-            }
+
+        // Nullify columns based on values in first rows
+        for(int column = 1; column < matrix[0].length; column++){
+            if(matrix[0][column] == 0) nullifyColumn(matrix, column);
+        }
+
+        // Nullify first row
+        if(rowHasZero) nullifyRow(matrix, 0);
+
+        // Nullify first column
+        if(columnHasZero) nullifyColumn(matrix, 0);
+
+    }
+
+    private void nullifyColumn(int[][] matrix, int column){
+        for(int r = 0; r < matrix.length; r++){
+            matrix[r][column] = 0;
+        }
+    }
+
+    private void nullifyRow(int[][] matrix, int row){
+        for(int col = 0; col < matrix[0].length; col++){
+            matrix[row][col] = 0;
         }
     }
 }
@@ -45,3 +83,29 @@ class Solution {
         }
     }
  */
+
+ /**
+    Second solution : using O(M+N) space
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> columns = new HashSet<>();
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == 0) {
+                    rows.add(i);
+                    columns.add(j);
+                }
+            }
+        }
+        for(int row : rows){
+            for(int col = 0; col < matrix[0].length; col++){
+                matrix[row][col] = 0;
+            }
+        }
+        for(int column : columns){
+            for(int r = 0; r < matrix.length; r++){
+                matrix[r][column] = 0;
+            }
+        }
+    }
+  */
