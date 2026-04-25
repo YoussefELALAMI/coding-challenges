c51033c310e577d0d8e138405412f4a4d998e6f6
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-04-25 12:35:44 +0200

    Time: 20 ms (5.16%), Space: 49.9 MB (5.59%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        List<List<Integer>> coordinates = new ArrayList<>();
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == 0) coordinates.add(new ArrayList<>(Arrays.asList(i,j)));
+            }
+        }
+        for(int c = 0; c < coordinates.size(); c++){
+            int row = coordinates.get(c).get(0);
+            int column = coordinates.get(c).get(1);
+            for(int col = 0; col < matrix[0].length; col++){
+                matrix[row][col] = 0;
+            }
+            for(int r = 0; r < matrix.length; r++){
+                matrix[r][column] = 0;
+            }
+        }
+    }
+}
+
+/**
+ First solution : using O(MxN) space
+ -> keeping the zero cells coordinates in an array
+ public void setZeroes(int[][] matrix) {
+        List<List<Integer>> coordinates = new ArrayList<>();
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == 0) coordinates.add(new ArrayList<>(Arrays.asList(i,j)));
+            }
+        }
+        for(int c = 0; c < coordinates.size(); c++){
+            int row = coordinates.get(c).get(0);
+            int column = coordinates.get(c).get(1);
+            for(int col = 0; col < matrix[0].length; col++){
+                matrix[row][col] = 0;
+            }
+            for(int r = 0; r < matrix.length; r++){
+                matrix[r][column] = 0;
+            }
+        }
+    }
+ */
