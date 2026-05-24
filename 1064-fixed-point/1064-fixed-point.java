ff61b9362f0b58d0f795049f336a7279836d6e52
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-05-24 12:14:14 +0200

    Time: 0 ms (100%), Space: 46.6 MB (52.69%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.java
+++ 1064-fixed-point/1064-fixed-point.java
@@ -1,8 +1,32 @@
 class Solution {
     public int fixedPoint(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            int diff = arr[mid] - mid;
+
+            if (diff == 0) {
+                result = mid;
+                right = mid - 1;
+            } else if (diff < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
+
+/**
+ O(n) Solution :
+     public int fixedPoint(int[] arr) {
         for(int i = 0; i < arr.length; i++){
             if(i==arr[i]) return i;
         }
         return -1;
     }
-}
+ */
