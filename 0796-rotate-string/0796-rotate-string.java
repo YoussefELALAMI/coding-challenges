215933414dd4d88e478748902c12d79000927ab0
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-05-24 11:38:38 +0200

    Time: 0 ms (100%), Space: 43 MB (26.44%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,6 +2,6 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         if(goal.length() != s.length()) return false;
         String concat = s + s;
-        return concat.contains(goal) ? true : false;
+        return concat.contains(goal);
     }
 }
