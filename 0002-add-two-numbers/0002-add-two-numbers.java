d7d89be2c4ee2b0bfc2347be62e28a99bc90f1af
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-08-12 15:23:18 +0200

    Time: 1 ms (100%), Space: 46.5 MB (38.19%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,6 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/**
+ * Recursive technique
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        return addTwoNumRecursive(l1, l2, carry);
+    }
+
+    private ListNode addTwoNumRecursive(ListNode l1, ListNode l2, int carry){
+        if (l1 == null && l2 == null && carry ==0) return null;
+        ListNode node = new ListNode(0);
+        int value = carry;
+        if(l1 != null) value += l1.val;
+        if(l2 != null) value += l2.val;
+        node.val = value % 10;
+        if (l1 != null || l2 != null) {
+            node.next = addTwoNumRecursive(l1 == null ? null : l1.next,
+                                        l2 == null ? null : l2.next,
+                                        value >= 10 ? 1 : 0);
+        }
+        return node;
+    }
+}
+
+/**
+ * A single-pass traversal with a dummy head node technique
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode beforehead = new ListNode(0);
@@ -26,3 +54,4 @@ class Solution {
         return beforehead.next;
     }
 }
+ */
