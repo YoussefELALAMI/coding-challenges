594ec6958f68a72c277e5741b8033bc32e318118
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-08-15 15:18:19 +0200

    Time: 2 ms (48.76%), Space: 53.3 MB (12.56%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,6 +11,31 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int diff = count(headA) - count(headB);
+        if (diff > 0) {
+            for(int i = 0; i < diff; i++) headA = headA.next;
+        } else if (diff < 0){
+            for(int i = Math.abs(diff); i > 0; i--) headB = headB.next;
+        } 
+        while (headA != null && headB != null) {
+            if(headA == headB) return headA;
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    private int count(ListNode node){
+        if(node == null) return 0;
+        return 1 + count(node.next);
+    }
+}
+
+
+
+/**
+ * HashTable solution :
+     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         HashSet<ListNode> nodes = new HashSet<>();
         while (headA != null) {
             nodes.add(headA);
@@ -22,4 +47,4 @@ public class Solution {
         }
         return null;
     }
-}
+ */
