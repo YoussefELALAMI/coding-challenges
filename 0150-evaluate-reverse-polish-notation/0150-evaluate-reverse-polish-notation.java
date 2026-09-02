b787ffb98cafdb3dad352a107a915d68a337caa6
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-09-02 22:20:44 +0200

    Time: 5 ms (96.91%), Space: 44.8 MB (98.49%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String s : tokens){
+            if(!isOperator(s)){
+                stack.push(Integer.parseInt(s));
+            } else {
+                char op = s.charAt(0);
+                int b = stack.pop();
+                int a = stack.pop();
+                switch (op) {
+                    case '+':
+                        stack.push(a + b);
+                        break;
+                    case '-':
+                        stack.push(a - b);
+                        break;
+                    case '*':
+                        stack.push(a * b);
+                        break;
+                
+                    default:
+                        stack.push(a / b);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    private boolean isOperator(String s) {
+        if (s == null || s.length() != 1) return false;
+        char c = s.charAt(0);
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+}
