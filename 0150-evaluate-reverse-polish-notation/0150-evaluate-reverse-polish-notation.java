60109fbde7ed9ce34a06ebb3cc12438d6cfa9375
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-09-02 22:25:07 +0200

    Time: 6 ms (87.29%), Space: 45.2 MB (63.21%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -2,35 +2,20 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         for(String s : tokens){
-            if(!isOperator(s)){
+            char c = s.charAt(0);
+            if(s.length() > 1 || Character.isDigit(c)){
                 stack.push(Integer.parseInt(s));
             } else {
-                char op = s.charAt(0);
                 int b = stack.pop();
                 int a = stack.pop();
-                switch (op) {
-                    case '+':
-                        stack.push(a + b);
-                        break;
-                    case '-':
-                        stack.push(a - b);
-                        break;
-                    case '*':
-                        stack.push(a * b);
-                        break;
-                
-                    default:
-                        stack.push(a / b);
-                        break;
+                switch (c) {
+                    case '+': stack.push(a + b); break;
+                    case '-': stack.push(a - b); break;
+                    case '*': stack.push(a * b); break;
+                    case '/': stack.push(a / b); break;
                 }
             }
         }
         return stack.pop();
-    }
-
-    private boolean isOperator(String s) {
-        if (s == null || s.length() != 1) return false;
-        char c = s.charAt(0);
-        return c == '+' || c == '-' || c == '*' || c == '/';
     }
 }
