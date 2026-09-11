81ba2172825bd4ae05f522198cddd9734dea92c2
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-09-11 11:42:28 +0200

    Time: 0 ms (100%), Space: 43 MB (70.02%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,12 +14,33 @@
  * }
  */
 class Solution {
-    List<Integer> output = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<>();
+        Deque<TreeNode> tree = new ArrayDeque<>();
         if(root == null) return output;
-        this.output.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        tree.push(root);
+        while(!tree.isEmpty()){
+            TreeNode curr = tree.pop();
+            output.add(curr.val);
+            if(curr.right != null) tree.push(curr.right);
+            if(curr.left != null) tree.push(curr.left);
+        }
         return output;
     }
 }
+
+
+
+/**
+    Recursive Approach :
+        class Solution {
+            List<Integer> output = new ArrayList<>();
+            public List<Integer> preorderTraversal(TreeNode root) {
+                if(root == null) return output;
+                this.output.add(root.val);
+                preorderTraversal(root.left);
+                preorderTraversal(root.right);
+                return output;
+            }
+        }
+ */
