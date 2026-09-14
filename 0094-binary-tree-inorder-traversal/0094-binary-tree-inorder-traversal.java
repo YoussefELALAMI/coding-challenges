d99167a21a972271b25a9f9b5d5bf970a620223c
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-09-14 15:45:43 +0200

    Time: 0 ms (100%), Space: 43.3 MB (32.7%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,12 +14,34 @@
  * }
  */
 class Solution {
-    List<Integer> output = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return output;
-        inorderTraversal(root.left);
-        output.add(root.val);
-        inorderTraversal(root.right);
+        List<Integer> output = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode curr = root;
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            output.add(curr.val);
+            curr = curr.right;
+        }
         return output;
     }
 }
+
+
+/**
+    Recursive Approach :
+    class Solution {
+        List<Integer> output = new ArrayList<>();
+        public List<Integer> inorderTraversal(TreeNode root) {
+            if(root == null) return output;
+            inorderTraversal(root.left);
+            output.add(root.val);
+            inorderTraversal(root.right);
+            return output;
+        }
+    }
+ */
