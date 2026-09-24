3361cebc1a236132cdba9e55c5cfc0f452c5540e
Youssef EL ALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-09-24 15:59:31 +0200

    Time: 0 ms (100%), Space: 42.7 MB (85.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,15 +15,9 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean result = true;
         if(p == null && q == null) return true;
-        else if((p == null && q != null) 
-                || (q == null && p != null)){
-                    return false;
-        }
+        else if(p == null || q == null) return false;
         else if(p.val != q.val) return false;
-        result = result && isSameTree(p.left, q.left);
-        result = result && isSameTree(p.right, q.right);
-        return result;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
